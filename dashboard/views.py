bcd35d785deab85855f7d57c41506a6ea14f76b3
Aliaa Hamad <125764832+Aliaaxx@users.noreply.github.com>
2025-05-23 00:06:26 +0300

    Add files via upload

---
+++ dashboard/views.py
@@ -0,0 +1,106 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from .models import Product
+from django.contrib.auth.decorators import login_required
+from .models import Product, Order
+from .forms import ProductForm, OrderForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+# Create your views here.
+@login_required
+def index(request):
+    orders = Order.objects.all()
+    if request.method == 'POST' :
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            isinstance = form.save(commit=False)
+            isinstance.staff = request.user
+            isinstance.save()
+            return redirect('dashboard-index')
+    else:
+        form = OrderForm()
+    context = {
+        'orders' : orders,
+        'form' : form,
+    }
+    return render(request, 'dashboard/index.html', context)
+
+@login_required
+def staff(request):
+    workers = User.objects.all()
+    context = {
+        'workers' : workers
+    }
+    return render(request, 'dashboard/staff.html', context)
+
+@login_required
+def staff_detail(request, pk):
+    worker = User.objects.get(id=pk)
+    context = {
+        'worker': worker
+    } 
+    return render(request, 'dashboard/staff_detail.html', context)
+
+
+@login_required
+def product(request):
+    items = Product.objects.all() # using ORM
+
+    # if you want to write normal SQL quieries you can use Raw
+    # to retrive particular table name of application _ name of model
+    # items = Product.objects.raw('SELECT * FROM dashboard_product')
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            product_name = form.cleaned_data.get('name') # Graping the name
+            messages.success(request, f'{product_name} has been added')
+            return redirect('dashboard-product')
+    else:
+        form = ProductForm()
+
+    context ={
+        'items' : items,
+        'form' : form,
+    }
+    return render(request, 'dashboard/product.html', context)
+
+@login_required
+def product_delete(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('dashboard-product')
+    return render(request, 'dashboard/product_delete.html')
+
+@login_required
+def product_update(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = ProductForm(instance=item)
+
+    context ={
+        'form' : form,
+    }
+    return render(request, 'dashboard/product_update.html', context)
+
+
+@login_required
+def order(request):
+    orders = Order.objects.all()
+
+    context = {
+        'orders' : orders
+    }
+    return render(request, 'dashboard/order.html', context)
+
+# Add this missing function
+def redirect_to_dashboard(request):
+    return redirect('dashboard-index')
